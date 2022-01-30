90d0c5a0af7c59bb474ffc6eb67dfeb54e941c32
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-01-30 21:54:57 +0200

    fix

--- middlewares/auth/authMiddleware.js
+++ middlewares/auth/authMiddleware.js
@@ -1,35 +1,30 @@
 const jwt = require('jsonwebtoken')
+const createError = require('http-errors')
 const { User } = require('../../models')
 const { constants: { ERROR_MESSAGES } } = require('../../utils')
 
 const SECRET_KEY = process.env.SECRET
 
 const auth = async (req, res, next) => {
-  try {
-    const { authorization } = req.headers
+  const { authorization } = req.headers
 
-    if (!authorization) {
-      throw new Error()
-    }
-
-    const token = authorization.split(' ')[1]
-    const result = jwt.verify(token, SECRET_KEY)
-
-    const user = await User
-      .findById(result.id)
-      .select({ email: 1, name: 1, avatarUrl: 1, token: 1 })
-
-    if (!user || token !== user.token) {
-      throw new Error()
-    }
-
-    req.user = user
-    next()
-  } catch (err) {
-    err.status = 401
-    err.message = ERROR_MESSAGES.notAuthorized
-    next(err)
+  if (!authorization) {
+    next(createError(401, ERROR_MESSAGES.notAuthorized))
   }
+
+  const token = authorization.split(' ')[1]
+  const result = jwt.verify(token, SECRET_KEY)
+
+  const user = await User
+    .findById(result.id)
+    .select({ email: 1, name: 1, avatarUrl: 1, token: 1 })
+
+  if (!user || token !== user.token) {
+    next(createError(401, ERROR_MESSAGES.notAuthorized))
+  }
+
+  req.user = user
+  next()
 }
 
 module.exports = auth
--- routes/userRouter/userRouter.js
+++ routes/userRouter/userRouter.js
@@ -7,7 +7,7 @@ const { wrapper } = require('../../utils')
 router.post('/signup', wrapper(controllers.signupUser))
 router.post('/login', wrapper(controllers.loginUser))
 
-router.use(middlewars.auth)
+router.use(wrapper(middlewars.auth))
 
 router.post('/logout', wrapper(controllers.logoutUser))
 router.get('/current', wrapper(controllers.currentUser))
