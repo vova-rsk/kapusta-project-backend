2df3f0ff48dae6eed73a99ee899fe10755cb47f0
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-01-30 21:07:19 +0200

    fix

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
+const { userRouter } = require('./routes')
+const { constants: { ERROR_MESSAGES } } = require('./utils')
 
 const app = express()
 
@@ -11,12 +13,14 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/user', userRouter)
+
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: ERROR_MESSAGES.notFound })
 })
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err
+  const { status = 500, message = ERROR_MESSAGES.serverError } = err
 
   res.status(status).json({
     status: 'error',
