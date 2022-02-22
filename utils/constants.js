2b2127b5dd981b0bdf918556f68250c479742687
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-02-22 21:16:34 +0200

    fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,20 @@
+const CATEGORY_TYPES = {
+  INCOME: 'income',
+  COST: 'cost'
+}
+
+const TRANSACTION_TYPES = {
+  ALL: 'all',
+  DEBIT: 'debit',
+  CREDIT: 'credit'
+}
+
+const REQ_VALIDATION_TARGET = {
+  BODY: 'body',
+  QUERY: 'query',
+  PARAMS: 'params'
+}
+
 const SUCCESS_MESSAGES = {
   dbConnectionSuccess: 'Database connection successful',
   serverStartedSuccess: 'Server running. Use our API on port'
@@ -35,53 +52,6 @@ const ERROR_MESSAGES = {
   invalidDescriptionValue: 'Invalid "description" field value',
   missingCategoryField: 'missing required "category" field',
   invalidCategoryValue: 'invalid category field value'
-}
-
-// const ERROR_MESSAGES = {
-//   missingTypeField: 'missing required type field',
-//   invalidTypeValue: 'invalid type value',
-//   missingDescriptionField: 'missing required description field',
-//   invalidDescriptionValue: 'invalid description value (must be between 2 and 20 characters, starting with letters and consisting of letters, numbers and spaces )',
-//   missingDateField: 'missing required date field',
-//   invalidDateFormat: 'invalid date format (must be in ISO 8601 format)',
-//   missingCategoryField: 'missing required category field',
-//   invalidCategoryFormat: 'invalid category format',
-//   missingAmountField: 'missing required amount field',
-//   invalidAmountFormat: 'invalid amount format',
-//   missingOwnerField: 'missing required owner field',
-//   invalidOwnerFormat: 'invalid owner format',
-//   missingIdQueryString: 'missing required query string of transactionId',
-//   invalidIdQueryStringFormat: 'invalid transactionId query string format',
-//   missingTypeQueryString: 'missing required query string of type',
-//   invalidTypeQueryStringFormat: 'invalid transactionId query string format',
-//   missingPeriodQueryString: 'missing required query string of period',
-//   invalidPeriodQueryStringFormat: 'invalid period query string format',
-//   outOfRangePeriodQueryString: 'period query string data is out of range (1900-9999)',
-//   missingYearQueryString: 'missing required query string of year',
-//   invalidYearQueryStringFormat: 'invalid year query string format',
-//   outOfRangeYearQueryString: 'year query string data is out of range (1900-9999)',
-//   invalidMonthQueryStringFormat: 'invalid month query string format',
-//   missingMonthQueryString: 'missing required query string of month',
-//   outOfRangeMonthQueryString: 'month query string data is out of range (0-11)',
-//   missingNameField: 'missing required name field',
-//   invalidNameValue: 'invalid name value (must be between 2 and 20 characters, starting with letters and consisting of letters, commas and spaces )'
-// }
-
-const CATEGORY_TYPES = {
-  INCOME: 'income',
-  COST: 'cost'
-}
-
-const TRANSACTION_TYPES = {
-  ALL: 'all',
-  DEBIT: 'debit',
-  CREDIT: 'credit'
-}
-
-const REQ_VALIDATION_TARGET = {
-  BODY: 'body',
-  QUERY: 'query',
-  PARAMS: 'params'
 }
 
 module.exports = {
